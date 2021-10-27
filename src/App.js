2afff5e4c7158a2396c8c3f0c81c2c5bb2f84a64
Jheser Guzman <dicotips@gmail.com>
2021-10-26 17:43:50 -0700

    Added login form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 import React from "react";
-// TODO: import useFormik from formik library
+import {useFormik} from 'formik'
 
 function App() {
-  // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      emailField: '',
+      passwordField: ''
+    },
+    onSubmit: values => {
+      console.log('form:', values);
+    },
+    validate: values  => {
+      let errors = {};
+      if (!values.emailField) {
+        errors.emailError = "Email is Required";
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.emailField)) {
+        errors.emailError = 'Username should be an email';
+      };
+      if (!values.passwordField) {
+        errors.pswError = "Field Required";
+      }
+      if (values.passwordField && values.emailField) {
+        alert("Login Successful");
+      }
+      return errors;
+
+     }
+  });
 
   return (
     <div>
-      <p>
-        The app is ready! You can proceed with the task instructions. TODO:
-        build you form here.
-      </p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Email</div>
+        <input
+          id="emailField"
+          type="email"
+          onSubmit={formik.handleChange}
+          value={formik.values.emailError}
+        />
+        {formik.errors.emailError ? (
+          <div style={{ color: "red" }}>{formik.errors.emailError}</div>
+        ) : null}
+        <div>Password</div>
+        <input
+          id="passwordField"
+          type="password"
+          onSubmit={formik.handleChange}
+          value={formik.values.pswError}
+        />
+        {formik.errors.pswError ? (
+          <div style={{ color: "red" }}>{formik.errors.pswError}</div>
+        ) : null}
+        <button type="submit" id="submitBtn">Submit</button>
+      </form>
     </div>
   );
 }
